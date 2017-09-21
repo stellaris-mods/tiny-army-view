95bf6c29429e54b154ef4381b85fc0d6027360de
folknor <folk@folk.wtf>
2017-09-21 22:10:22 +0200

    Initial upload for Stellaris 1.6.x or earlier.

---
+++ modinfo.lua
@@ -0,0 +1,9 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "folk_tinyarmy",
+	name = "Tiny Army View",
+	tags = { "Utilities", "Graphics" },
+	picture = "thumb.png",
+	supported_version = "1.6.*",
+	remote_file_id = 836295988,
+}
