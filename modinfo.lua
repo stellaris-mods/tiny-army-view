f43b3081543a2b6234a9d1cf4efe01d9e64ce8ce
folknor <folk@folk.wtf>
2017-09-22 12:54:46 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,6 +4,6 @@ return {
 	name = "Tiny Army View",
 	tags = { "Utilities", "Graphics" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 836295988,
 }
